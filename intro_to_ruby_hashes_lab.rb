01389ab40ebf96e9ede2170dc6bdb90416bf8834
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-19 02:28:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       names: {
-        reading_railroads: {},
-        pennsylvania_railroads: {},
-        b_and_o_railroads: {},
+        reading_railroad: {},
+        pennsylvania_railroad: {},
+        b_and_o_railroad: {},
         shortline: {}
       },
       rent_in_dollars: {
@@ -46,16 +46,17 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	railroads: {
+	monopoly = {
+	  railroads: {
       pieces: 4,
       names: {
-        reading_railroads: {
+        reading_railroad: {
           "mortgage_value" => "$100"
           },
-        pennsylvania_railroads: {
+        pennsylvania_railroad: {
           "mortgage_value" => "$200"
           },
-          b_and_o_railroads: {
+          b_and_o_railroad: {
             "mortgage_value" => "$400"
           },
           shortline: {
